6eb39cba7cb5c41c7c3bb5e06bc1404754b9320b
nb89portfolio <navraj.bains.89@outlook.com>
2024-02-26 18:39:13 -0500

    Extended error fallback feedback and ability to reset.

--- app/error/fallback.tsx
+++ app/error/fallback.tsx
@@ -1,12 +1,38 @@
+'use client';
+
 import { FallbackProps } from 'react-error-boundary';
+
+function defineError(error: any): {
+	name: string;
+	message: string;
+	stack: string;
+} {
+	const isTypeError = error instanceof Error;
+
+	return {
+		name: isTypeError ? error.name : 'Error name is unknown.',
+		message: isTypeError ? error.message : 'Error name is unknown.',
+		stack: isTypeError
+			? error.stack !== undefined
+				? error.stack
+				: 'Error stack is undefined.'
+			: 'Error name is unknown.',
+	};
+}
 
 export default function ErrorFallback({
 	error,
 	resetErrorBoundary,
 }: FallbackProps) {
+	const { name, message, stack } = defineError(error);
+
 	return (
 		<main>
 			<h2>Error</h2>
+			<p>{name}</p>
+			<p>{message}</p>
+			<details>{stack}</details>
+			<button onClick={resetErrorBoundary}>Reset Error</button>
 		</main>
 	);
 }
