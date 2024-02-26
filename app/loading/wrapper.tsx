9b3215552ae8919b1aa1acee40d95dc597fa4954
nb89portfolio <navraj.bains.89@outlook.com>
2024-02-26 12:06:13 -0500

    Loading wrapper has been added.

---
+++ app/loading/wrapper.tsx
@@ -0,0 +1,10 @@
+import { ReactNode, Suspense } from 'react';
+import LoadingFallback from './fallback';
+
+export default function LoadingWrapper({ children }: { children: ReactNode }) {
+	return (
+		<Suspense fallback={<LoadingFallback></LoadingFallback>}>
+			{children}
+		</Suspense>
+	);
+}
