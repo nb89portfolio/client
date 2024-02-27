c4e8b9d68718faf69e24b279f88285c2431251d0
nb89portfolio <navraj.bains.89@outlook.com>
2024-02-27 10:33:29 -0500

    Added error api rout.

--- app/error/wrapper.tsx
+++ app/error/wrapper.tsx
@@ -1,11 +1,37 @@
-import { ErrorInfo, ReactNode } from 'react';
+'use client';
+
+import { ErrorInfo, ReactNode, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './fallback';
 
-function logError(error: Error, info: ErrorInfo) {}
-
 export default function ErrorWrapper({ children }: { children: ReactNode }) {
+	const [getErrorRecord, setErrorRecord] = useState<
+		{ name: string; message: string; stack: string }[]
+	>([]);
+
+	function logError(error: Error, info: ErrorInfo) {
+		const findDuplicate = getErrorRecord.find((errorRecord) => {
+			return error.name === errorRecord.name
+				? error.message === errorRecord.message
+					? error.stack === errorRecord.stack
+						? {
+								name: error.name,
+								message: error.message,
+								stack: error.stack,
+						  }
+						: undefined
+					: undefined
+				: undefined;
+		});
+
+		const hasDuplicate = findDuplicate !== undefined;
+
+		if (!hasDuplicate) {
+		}
+	}
+
 	return (
+		
 		<ErrorBoundary
 			FallbackComponent={ErrorFallback}
 			onError={logError}>
