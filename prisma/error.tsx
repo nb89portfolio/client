4e48ce9d0e68b83f777ce47f844c39a3dcf738fb
nb89portfolio <navraj.bains.89@outlook.com>
2024-03-08 16:40:15 -0500

    Added error prisma functions.

--- prisma/error.tsx
+++ prisma/error.tsx
@@ -1,9 +1,14 @@
 import { DefinedError } from '@/functions/definedError';
 import { DocumentedError, PrismaClient } from '@prisma/client';
 
+type PrismaErrorResponse = {
+	data: DocumentedError | null;
+	validated: boolean;
+};
+
 export async function prismaFindDuplicateError(
 	request: DefinedError
-): Promise<{ data: DocumentedError | null; validated: boolean }> {
+): Promise<PrismaErrorResponse> {
 	const prismaClient = new PrismaClient();
 
 	const data = await prismaClient.documentedError.findFirst({
@@ -15,6 +20,46 @@ export async function prismaFindDuplicateError(
 	});
 
 	const validated = data !== null;
+
+	return { data, validated };
+}
+
+export async function prismaUpdateDocumentedError(
+	request: DocumentedError
+): Promise<PrismaErrorResponse> {
+	const prismaClient = new PrismaClient();
+
+	const data = await prismaClient.documentedError.update({
+		where: {
+			id: request.id,
+		},
+		data: {
+			updated: new Date(),
+			priority: request.priority + 1,
+		},
+	});
+
+	const validated = data !== null || data !== undefined;
+
+	return { data, validated };
+}
+
+export async function prismaCreateDocumentedError(
+	request: DocumentedError
+): Promise<PrismaErrorResponse> {
+	const prismaClient = new PrismaClient();
+
+	const data = await prismaClient.documentedError.create({
+		data: {
+			name: request.name,
+			message: request.message,
+			stack: request.stack,
+			created: new Date(),
+			updated: new Date(),
+		},
+	});
+
+	const validated = data !== null || data !== undefined;
 
 	return { data, validated };
 }
